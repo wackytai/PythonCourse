3bac87f677f484cd0d1b03a4cc213d5bae8d7da1
Tainah Monteiro <tainahmonteiro.info@gmail.com>
2025-05-24 17:26:17 +0100

    ex02

---
+++ basics/ex02.py
@@ -0,0 +1,8 @@
+#'...' is an ellipse and can be used as placeholder so your code doesn't have errors
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / (altura ** 2)
+
+print(nome + " tem " + str(altura) + " de altura,", "pesa " + str(peso) +
+	  " quilos e seu IMC: é", imc, sep='\n')
---
+++ basics/operators.py
@@ -0,0 +1,23 @@
+# add: +
+#	if you add int with a float, the result will be a float
+# subtract: -
+# multiply: *
+# power: **
+# true division: / (retuns a floating-point result)
+# floor division: // (returns largest integer less than or equal to the result)
+# module: %
+# concat:
+concat = 'Luiz' + ' ' + 'Miranda'
+print(concat)
+
+# repetition:
+char = 'A'
+
+print(char * 10)
+
+#operations priority:
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# if you have several operators with same piro, it starts from the left
+# 4. * -
