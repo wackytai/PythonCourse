ebda5bb2a22c02f16f840568af957f42261d5500
Tainah Monteiro <tainahmonteiro.info@gmail.com>
2025-05-24 12:59:04 +0100

    class 01

---
+++ basics/class_1.py
@@ -0,0 +1,13 @@
+'''
+This is a docString, they are a sort of multiline comment.
+Use it with caution, beucase python compiler will read it 
+and store it in memoryalthough not being executable
+'''
+# print() function, space is the default separator between its arguments,
+# but by using sep=<string> you can change it
+#you can also change its ending instead of breakline, by using end=<string>
+#print(123,34, end="#", sep="0")
+#custom settings must always come as last arguments of the function
+#print(sep=';', 123,34) WILL GIVE YOU AN ERROR
+print(123,34)
+print(123,34, sep="--")
