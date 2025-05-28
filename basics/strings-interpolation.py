de9976d0185b0923ce8afbe33937426a4da2795d
Tai Monteiro <48991630+wackytai@users.noreply.github.com>
2025-05-28 17:38:19 +0100

    f-strings formatting

--- basics/strings-interpolation.py
+++ basics/strings-interpolation.py
@@ -4,3 +4,17 @@ price = 1000.938745984
 var = "%s, o preço é R$%.2f" % (name, price)
 print(var)
 print("O hexadecimal de %d é %08X" % (1500, 1500))
+
+# -----------------------------------------------------------------------
+# basics of string formatting: f-strings:
+var = "ABC"
+print(f"-{var}-")
+print(f"-{var:$>10}-")
+print(f"-{var:$<10}-")
+print(f"-{var:$^10}-")
+print(f"{1000.4283748726497392:0>+10,.1f}")
+print(f"{1000.4283748726497392:0=+10,.1f}")
+print(f"O hexadecimal de 1500 é {1500:08X}")
+print(f"{var!r}")
+print(f"{var!s}")
+print(f"{var!a}")
