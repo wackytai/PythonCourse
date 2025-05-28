5e5419f829f280ae2263ad4e47e5e994eb168033
Tai Monteiro <48991630+wackytai@users.noreply.github.com>
2025-05-28 18:01:41 +0100

    ex04

---
+++ basics/ex04.py
@@ -0,0 +1,15 @@
+name = input("Name: ")
+age = input("Age: ")
+isSpacedName = False
+
+if not name or not age:
+	print("Sorry, you left at least one field empty")
+else:
+	if ' ' in name:
+		isSpacedName = True
+	print(f"Your name is {name}")
+	print("Your inverted name is", name[::-1])
+	print("Does your name have space?", isSpacedName)
+	print(f"Your name has {len(name)}")
+	print(f"Your name's first letter is {name[0]}")
+	print(f"Your name's last letter is {name[len(name)-1]}")
