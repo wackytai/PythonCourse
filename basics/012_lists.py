1c58612d6063c629cee473647016dc52fc489bb8
wackytai <tainahmonteiro.info@gmail.com>
2025-12-14 16:55:52 +0000

    lists

---
+++ basics/012_lists.py
@@ -0,0 +1,13 @@
+# Lists in Python [aka arrays from other programming languages]
+# List type is mutable!
+# Supports values of any kind, indexes and slicing
+# Helpful methods: append, insert, pop, del, clear, extend, CRUD
+# If list is empty, bool(lst) is gonna return False
+# Lists in Pyhton access values through pointers
+
+string = "ABCDE" # len = 5 chars
+lst =  [10, 20, 30, 40, 55]
+#print(bool(lst))
+#print(len(lst[2]))
+print(len(lst))
+print(lst, len(lst))
